91ff4c3c243fff59a5aea42d27b2374001b71a60
sahare9422 <palash.sahare6868@gmail.com>
2025-04-27 03:03:06 +0530

    key added

--- compute/main.tf
+++ compute/main.tf
@@ -19,7 +19,7 @@ data "aws_ami" "linux" {
 resource "aws_launch_template" "web-batch891" {
   name_prefix            = "web-batch891"
   image_id               = data.aws_ami.linux.id
-  key_name               = "batch-910"
+  key_name               = "newus"
   instance_type          = var.web_instance_type
   vpc_security_group_ids = [var.web_sg]
   user_data              = filebase64("install_apache.sh")
